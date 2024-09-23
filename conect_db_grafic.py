94bdef59d62b00ed07f39abb097dab386bbd21ee
AlmironDev <almironbill@gmail.com>
2024-09-23 12:09:40 -0500

    -

--- conect_db_grafic.py
+++ conect_db_grafic.py
@@ -10,6 +10,7 @@ app = Flask(__name__)
 def get_db_connection():
     conn = mysql.connector.connect(
         host='localhost',
+        port=3306,
         user='root',
         password='1775',
         database='pythonprueba'
